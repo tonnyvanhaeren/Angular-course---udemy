22917df56fdc41819e64dc7788b92cc722fdbd55
antonius-vanhaeren <antonius.vanhaeren.av@gmail.com>
2026-05-18 17:49:45 +0200

    seven - schare NgModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,21 @@
 import localeNlBe from '@angular/common/locales/nl-BE';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TaskComponent } from './tasks/task/task.component';
 import { registerLocaleData } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 registerLocaleData(localeNlBe);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    CardComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-  ],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  imports: [BrowserModule, SharedModule, TasksModule],
   providers: [{ provide: LOCALE_ID, useValue: 'nl-BE' }],
   bootstrap: [AppComponent],
 })
